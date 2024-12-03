29c4081d632570f69392bfa22d83b873596a6e5e
ibrahim kebbeh <engineeredlie@gmail.com>
2024-12-03 17:42:14 -0500

    Merge remote README with local project

---
+++ src/main/java/com/hikingapp/trailwise/TrailWiseApplication.java
@@ -0,0 +1,14 @@
+package com.hikingapp.trailwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailWiseApplication {
+
+	public static void main(String[] args) {
+		System.out.println("null");
+		SpringApplication.run(TrailWiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hikingapp/trailwise/TrailWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikingapp.trailwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
